a77e97f954a9073cc68f1bf3010551f3db873991
Marcos Martinez <marcos.martinez.romero@gmail.com>
2019-08-11 20:03:18 -0700

    Rename project to ClickLock

---
+++ ClickLock/AutoLaunchHelper/HelperAppDelegate.swift
@@ -0,0 +1,37 @@
+//
+//  AppDelegate.swift
+//  AutoLaunchHelper
+//
+//  Created by marcosmr on 8/11/19.
+//  Copyright © 2019 marcosmr. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        let runningApps = NSWorkspace.shared.runningApplications
+        let isRunning = runningApps.contains {
+            $0.bundleIdentifier == "your.domain.TestAutoLaunch"
+        }
+        
+        if !isRunning {
+            var path = Bundle.main.bundlePath as NSString
+            for _ in 1...4 {
+                path = path.deletingLastPathComponent as NSString
+            }
+            NSWorkspace.shared.launchApplication(path as String)
+        }
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ ClickLock/AutoLaunchHelper/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  AutoLaunchHelper
+//
+//  Created by marcosmr on 8/11/19.
+//  Copyright © 2019 marcosmr. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
